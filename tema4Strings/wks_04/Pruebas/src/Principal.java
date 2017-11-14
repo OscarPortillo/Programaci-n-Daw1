d2fdc5ceb8ff98db497b62de7e4bcc9c6fa78270
OscarPortillo <oscar94.oscar@hotmail.com>
2017-11-14 20:09:11 +0100

    Prueba2 pull request

---
+++ tema4Strings/wks_04/Pruebas/src/Principal.java
@@ -0,0 +1,13 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		String s1="" ,s2=null;
+		s1=s1+s2;
+		
+		System.out.println(s1);	
+		s1="hola";
+		System.out.println(s1+" "+s2);
+	}
+
+}
