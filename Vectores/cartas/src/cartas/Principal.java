bc3a0c7293f145f7a7c88ba2ffd7b3e1461b70d3
OscarPortillo <oscar94.oscar@hotmail.com>
2017-11-30 18:12:27 +0100

    jaja

--- Vectores/cartas/src/cartas/Principal.java
+++ Vectores/cartas/src/cartas/Principal.java
@@ -1,27 +1,31 @@
 package cartas;
  
+
 public class Principal {
 
 	public static void main(String[] args) {
-		
+
 		String barajaOriginal[] = { "1♦","2♦","3♦","4♦","5♦","6♦","7♦","8♦","9♦","10♦",
-									"1♥","2♥","3♥","4♥","5♥","6♥","7♥","8♥","9♥","10♥",
-									"1♣","2♣","3♣","4♣","5♣","6♣","7♣","8♣","9♣","10♣",
-									"1♠","2♠","3♠","4♠","5♠","6♠","7♠","8♠","9♠","10♠",
-								  };
-	
+				"1♥","2♥","3♥","4♥","5♥","6♥","7♥","8♥","9♥","10♥",
+				"1♣","2♣","3♣","4♣","5♣","6♣","7♣","8♣","9♣","10♣",
+				"1♠","2♠","3♠","4♠","5♠","6♠","7♠","8♠","9♠","10♠",
+		};
+
 		int cartas = barajaOriginal.length; 
-		
+
 		while (cartas >0)
 		{
-		int pos = (int) (Math.random() * cartas);
-		System.out.println(barajaOriginal[pos]);
-		barajaOriginal[pos] = barajaOriginal[cartas-1];
-		cartas--;
+			int pos = (int) (Math.random() * cartas);
+			System.out.println(barajaOriginal[pos]);
+			barajaOriginal[pos] = barajaOriginal[cartas-1];
+			cartas--;
 		}
-		
-		
-		
+
+
+
+ 
+
+
 	}//
 
 }//
